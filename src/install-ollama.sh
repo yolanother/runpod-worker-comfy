c69c6bf30708ba26299685c6c6d2da0b2f52b0c9
Yolan <yolanother@gmail.com>
2025-02-01 22:01:42 -0800

    Remove unneeded sudo

--- src/install-ollama.sh
+++ src/install-ollama.sh
@@ -1,4 +1,4 @@
 echo "Installing Ollama..."
 
 curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz
-sudo tar -C /usr -xzf ollama-linux-amd64.tgz
+tar -C /usr -xzf ollama-linux-amd64.tgz
